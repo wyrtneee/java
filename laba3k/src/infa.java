794c993b649825325d3e7884b318eea22e909bab
wyrtneee <147799833+wyrtneee@users.noreply.github.com>
2023-12-18 20:17:06 +0300

    Add files via upload

---
+++ laba3k/src/infa.java
@@ -0,0 +1,12 @@
+
+
+public class infa {
+    private final String surname = "Герасименя";
+    private final int group = 5;
+
+    @Override
+    public String toString() {
+        return "Фамилия " + surname + " " +
+                ", Группа " + group;
+    }
+}
